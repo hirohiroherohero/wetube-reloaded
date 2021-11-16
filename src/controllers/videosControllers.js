7b6b593be9f457a33c367523fa8a2c10d2e5557a
hirohiroherohero <70766495+hirohiroherohero@users.noreply.github.com>
2021-11-17 00:05:59 +0900

    21.11.17
    Conditionals, Iteration, Mixins
    간단 render, absolute / relative url

--- src/controllers/videosControllers.js
+++ src/controllers/videosControllers.js
@@ -1,5 +1,41 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("watch");
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Video #2",
+    title: "Second Video",
+    rating: 2,
+    comments: 3,
+    createdAt: "2 minutes ago",
+    views: 12,
+    id: 2,
+  },
+  {
+    title: "Whatsup",
+    title: "Third Video",
+    rating: 1,
+    comments: 42,
+    createdAt: "2 minutes ago",
+    views: 49,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+export const watch = (req, res) => {
+  const { id } = req.params;
+  // = const id = req.params.id 위의 방법은
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching ${video.title}`, video });
+};
 export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search Video");
 export const upload = (req, res) => res.send("Upload Video");
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,6 +1,6 @@
 import express from "express";
 import {
-  see,
+  watch,
   edit,
   upload,
   deleteVideo,
@@ -10,7 +10,7 @@ const videoRouter = express.Router();
 
 videoRouter.get("/upload", upload);
 
-videoRouter.get("/:id(\\d+)", see);
+videoRouter.get("/:id(\\d+)", watch);
 videoRouter.get("/:id(\\d+)/edit", edit);
 videoRouter.get("/:id(\\d+)/delete", deleteVideo);
 
